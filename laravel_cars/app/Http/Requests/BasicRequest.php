1a4ee1398c7f6708f1d6297b2a51b54a42c4f4a7
B218TAN-11 <B218TAN-11@boronkay.local>
2025-02-25 13:51:57 +0100

    jo_checkolas

--- laravel_cars/app/Http/Requests/BasicRequest.php
+++ laravel_cars/app/Http/Requests/BasicRequest.php
@@ -26,15 +26,28 @@ class BasicRequest extends FormRequest
         }
         return [
             'name' => 'required|min:3|max:255',
+            'registration_number' => 'required|min:7|max:7',
+            'VIN' => 'required|min:17|max:17',
+            'maker_ID' => 'required',
+            'body_ID' => 'required',
+            
         ];
     }
 
     public function messages(): array
     {
         return [
-            'name.required' => 'Kötelező mező!',
+            'name.required' => 'A megnevezés kötelező mező!',
             'name.min' => 'Legalább 3 karakter legyen!',
             'name.max' => 'Legfeljebb 255 karakter legyen!',
+            'registration_number.required' => 'A rendszám kötelező mező!',
+            'registration_number.min' => 'A rendszám pontosan 7 karakter legyen!',
+            'registration_number.max' => 'A rendszám pontosan 7 karakter legyen!',
+            'VIN.required' => 'A jármű azonosító száma kötelező mező!',
+            'VIN.min' => 'A jármű azonosító száma pontosan 17 karakter legyen!',
+            'VIN.max' => 'A jármű azonosító száma pontosan 17 karakter legyen!',
+            'maker_ID.required' => 'A gyártó ID-je kötelező mező!',
+            'body_ID.required' => 'Az alváz ID-je kötelező mező!'
         ];
     }
 }
